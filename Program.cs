5a1a005b087a4697910c486d686d5bdcaf9160a4
Yohans Ashine <yohansabigail@yahoo.com>
2026-07-10 05:37:12 -0400

    First Program

--- Program.cs
+++ Program.cs
@@ -1,34 +1,12 @@
 ﻿using System;
-using System.Runtime.CompilerServices;
 
-class Program
+namespace MyApp
 {
-    protected static int IntInput(string label)
+    class Program
     {
-        Console.WriteLine($"Please enter a valid integer for {label}");
-        string? input = Console.ReadLine();
-        if (string.IsNullOrWhiteSpace(input) || input == null)
+        static void Main(string[] args)
         {
-            throw new ArgumentException("Input cannot be null or empty");
+            Console.WriteLine("Hello, World!");
         }
-        if(int.TryParse(input,out int result))
-        {
-            if(result > 15)
-            {
-                throw new ArgumentException("Number too high!");
-            }else if(result < 1)
-            {
-                throw new ArgumentException("Number too low!");
-            }
-            Console.WriteLine($"You entered: {result}");
-            return result;
-        }
-        throw new ArgumentException("Not a valid integer");
     }
-     static void Main(string[] args)
-    {
-        int first = IntInput("These Nuts: ");
-        Console.WriteLine(first);
-    }
-     
 }
